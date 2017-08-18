c85d48da8c3e2da03a368680c3be470ac17ed3b8
xClouder <xclouder@qq.com>
2017-08-18 14:24:09 +0800

    compile error

---
+++ Assets/DemoScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DemoScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//compile faile here
+		as =1
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
